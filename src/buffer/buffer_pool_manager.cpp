e0eb9e14f9ec9047b300264682e67e46a0f1c60a
i-cannot <148732419+i-cannot@users.noreply.github.com>
2025-05-15 17:46:45 +0800

    Add files via upload

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -26,56 +26,215 @@ BufferPoolManager::~BufferPoolManager() {
  * TODO: Student Implement
  */
 Page *BufferPoolManager::FetchPage(page_id_t page_id) {
-  // 1.     Search the page table for the requested page (P).
-  // 1.1    If P exists, pin it and return it immediately.
-  // 1.2    If P does not exist, find a replacement page (R) from either the free list or the replacer.
-  //        Note that pages are always found from the free list first.
-  // 2.     If R is dirty, write it back to the disk.
-  // 3.     Delete R from the page table and insert P.
-  // 4.     Update P's metadata, read in the page content from disk, and then return a pointer to P.
-  return nullptr;
+  std::scoped_lock<std::recursive_mutex> lock(latch_);
+
+  // 1. Search the page table for the requested page (P).
+  auto it = page_table_.find(page_id);
+  if (it != page_table_.end()) {
+    // 1.1 If P exists, pin it and return it immediately.
+    frame_id_t frame_id = it->second;
+    Page *page = &pages_[frame_id];
+    page->pin_count_++;
+    replacer_->Pin(frame_id);
+    return page;
+  }
+
+  // 1.2 If P does not exist, find a replacement page (R) from either the free list or the replacer.
+  frame_id_t frame_id;
+  if (!TryToFindFreePage(&frame_id)) {
+    return nullptr;
+  }
+
+  Page *replacement_page = &pages_[frame_id];
+
+  // 2. If R is dirty, write it back to the disk.
+  if (replacement_page->IsDirty()) {
+    disk_manager_->WritePage(replacement_page->GetPageId(), replacement_page->GetData());
+    replacement_page->is_dirty_ = false;
+  }
+
+  // 3. Delete R from the page table and insert P.
+  if (replacement_page->GetPageId() != INVALID_PAGE_ID) {
+    page_table_.erase(replacement_page->GetPageId());
+  }
+
+  // Insert P into the page table
+  page_table_[page_id] = frame_id;
+
+  // 4. Update P's metadata, read in the page content from disk, and then return a pointer to P.
+  replacement_page->ResetMemory();
+  replacement_page->page_id_ = page_id;
+  replacement_page->pin_count_ = 1;
+  replacement_page->is_dirty_ = false;
+
+  disk_manager_->ReadPage(page_id, replacement_page->GetData());
+
+  return replacement_page;
 }
 
 /**
  * TODO: Student Implement
  */
 Page *BufferPoolManager::NewPage(page_id_t &page_id) {
-  // 0.   Make sure you call AllocatePage!
-  // 1.   If all the pages in the buffer pool are pinned, return nullptr.
-  // 2.   Pick a victim page P from either the free list or the replacer. Always pick from the free list first.
-  // 3.   Update P's metadata, zero out memory and add P to the page table.
-  // 4.   Set the page ID output parameter. Return a pointer to P.
-  return nullptr;
+  
+  std::scoped_lock<std::recursive_mutex> lock(latch_);
+
+  // 1. If all the pages in the buffer pool are pinned, return nullptr.
+  frame_id_t frame_id;
+  if (!TryToFindFreePage(&frame_id)) {
+    return nullptr;
+  }
+
+  // 0. Allocate a new page
+  page_id = AllocatePage();
+  if (page_id == INVALID_PAGE_ID) {
+    return nullptr;
+  }
+
+  Page *new_page = &pages_[frame_id];
+
+  // 2. If R is dirty, write it back to the disk.
+  if (new_page->IsDirty()) {
+    disk_manager_->WritePage(new_page->GetPageId(), new_page->GetData());
+  }
+
+  // 3. Delete R from the page table and insert P.
+  if (new_page->GetPageId() != INVALID_PAGE_ID) {
+    page_table_.erase(new_page->GetPageId());
+  }
+
+  // Insert new page into the page table
+  page_table_[page_id] = frame_id;
+
+  // 4. Update P's metadata, zero out memory and add P to the page table.
+  new_page->ResetMemory();
+  memcpy(new_page->GetData(), EMPTY_PAGE_DATA, PAGE_SIZE);
+  new_page->page_id_ = page_id;
+  new_page->pin_count_ = 1;
+  new_page->is_dirty_ = false;
+
+  // 5. Return a pointer to P.
+  return new_page;
+  
+ 
 }
 
 /**
  * TODO: Student Implement
  */
 bool BufferPoolManager::DeletePage(page_id_t page_id) {
-  // 0.   Make sure you call DeallocatePage!
-  // 1.   Search the page table for the requested page (P).
-  // 1.   If P does not exist, return true.
-  // 2.   If P exists, but has a non-zero pin-count, return false. Someone is using the page.
-  // 3.   Otherwise, P can be deleted. Remove P from the page table, reset its metadata and return it to the free list.
-  return false;
+  std::scoped_lock<std::recursive_mutex> lock(latch_);
+
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end()) {
+    // 1. If P does not exist, return true.
+    return true;
+  }
+
+  frame_id_t frame_id = it->second;
+  Page *page = &pages_[frame_id];
+
+  // 2. If P exists, but has a non-zero pin-count, return false. Someone is using the page.
+  if (page->pin_count_ > 0) {
+    return false;
+  }
+
+  // 3. Otherwise, P can be deleted. Remove P from the page table, reset its metadata and return it to the free list.
+  page_table_.erase(page_id);
+  replacer_->Pin(frame_id); // Remove from replacer
+
+  page->ResetMemory();
+  page->page_id_ = INVALID_PAGE_ID;
+  page->pin_count_ = 0;
+  page->is_dirty_ = false;
+
+  free_list_.push_back(frame_id);
+
+  // Deallocate the page
+  DeallocatePage(page_id);
+
+  return true;
 }
 
 /**
  * TODO: Student Implement
  */
 bool BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty) {
-  return false;
+  std::scoped_lock<std::recursive_mutex> lock(latch_);
+
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end()) {
+    return false;
+  }
+
+  frame_id_t frame_id = page_table_[page_id];
+  Page *page = &pages_[frame_id];
+
+  //if (page->pin_count_ <= 0) {
+  //  return false;
+  //}
+  if (page->GetPinCount() == 0) {
+    return true; // 页面未被固定
+  }
+
+  page->pin_count_--;
+  if (page->pin_count_ == 0) {
+    replacer_->Unpin(frame_id);
+  }
+
+  if (is_dirty) {
+    page->is_dirty_ = true;
+  }
+
+  return true;
 }
 
 /**
  * TODO: Student Implement
  */
 bool BufferPoolManager::FlushPage(page_id_t page_id) {
-  return false;
+  std::scoped_lock<std::recursive_mutex> lock(latch_);
+
+  if (page_id == INVALID_PAGE_ID) {
+    return false;
+  }
+
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end()) {
+    return false;
+  }
+
+  frame_id_t frame_id = it->second;
+  Page *page = &pages_[frame_id];
+
+  disk_manager_->WritePage(page_id, page->GetData());
+  page->is_dirty_ = false;
+
+  return true;
 }
+
+bool BufferPoolManager::TryToFindFreePage(frame_id_t *frame_id) {
+  // First, try to find a page from the free list
+  if (!free_list_.empty()) {
+    *frame_id = free_list_.front();
+    free_list_.pop_front();
+    return true;
+  }
+
+  // If free list is empty, try to find a victim from the replacer
+  if (replacer_->Size() > 0) {
+    if (replacer_->Victim(frame_id)) {
+      return true;
+    }
+  }
+  //LOG(INFO) << "Free frame id: " << *frame_id << endl;
+  // No available page
+  return false;
+}    
 
 page_id_t BufferPoolManager::AllocatePage() {
   int next_page_id = disk_manager_->AllocatePage();
+  //LOG(INFO) << "Allocated page_id: " << next_page_id << endl;
   return next_page_id;
 }
 
--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -1,6 +1,6 @@
 #include "buffer/lru_replacer.h"
 
-LRUReplacer::LRUReplacer(size_t num_pages){}
+LRUReplacer::LRUReplacer(size_t num_pages) : size_(0), max_size_(num_pages){}
 
 LRUReplacer::~LRUReplacer() = default;
 
@@ -8,26 +8,75 @@ LRUReplacer::~LRUReplacer() = default;
  * TODO: Student Implement
  */
 bool LRUReplacer::Victim(frame_id_t *frame_id) {
-  return false;
+  std::scoped_lock<std::mutex> lock(mutex_);
+
+  // 如果链表为空，表示没有可替换的页面
+  if (size_ == 0) {
+    return false;
+  }
+
+  // 选择链表尾部的节点（最久未使用的页面）
+  *frame_id = free_list_.back();
+
+  // 从链表中移除
+  free_list_.pop_back();
+
+  // 删除节点
+  free_set_.erase(*frame_id);
+
+  // 减少当前可替换页面数
+  --size_;
+
+  return true;
 }
 
 /**
  * TODO: Student Implement
  */
 void LRUReplacer::Pin(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> lock(mutex_);
 
+  // 如果页面没有在链表中，说明它没有被 Unpin 或已经被 Pin，不需要处理
+  if (free_set_.find(frame_id) == free_set_.end()) {
+    return;
+  }
+  else {
+    // 如果页面在链表中，将其移除
+    free_list_.remove(frame_id);
+    free_set_.erase(frame_id);
+
+  // 减少当前可替换页面数
+  --size_;
+  }
+  
 }
 
 /**
  * TODO: Student Implement
  */
 void LRUReplacer::Unpin(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> lock(mutex_);
 
+  // 如果页面已经存在于链表中，不需要处理
+  if (free_set_.find(frame_id) != free_set_.end()) {
+    return;
+  }
+  else if (size_ >= max_size_) {
+    return;
+  }
+  else {
+    // 如果链表未满，将页面添加到链表头部
+    free_list_.push_front(frame_id);
+    free_set_.insert(frame_id);
+    size_++;
+  }
 }
 
 /**
  * TODO: Student Implement
  */
 size_t LRUReplacer::Size() {
-  return 0;
+  std::scoped_lock<std::mutex> lock(mutex_);
+  return size_;
 }
+
